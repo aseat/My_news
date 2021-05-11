212a10c6188ba8a17be8d355112e2966bb80904b
aseat <lemnlsdrye231mk@gmail.com>
2021-05-11 23:12:10 +0900

    テストコード

--- src/backend/tests/Feature/PostTest.php
+++ src/backend/tests/Feature/PostTest.php
@@ -20,15 +20,24 @@ class PostTest extends TestCase
         $post->title = "aaaa";
         $post->text = "bbb";
         $post->user_id = "1";
+        $post->image_path = "https://res.cloudinary.com/dueb2i48f/image/upload/c_fit,h_200,w_200/zsxizljjti5fxlfyklim.png";
         $post->save();
 
         $readPost = \App\Post::where('title', 'aaaa')->first();
         $this->assertNotNull($readPost);
-        // $readPost = \App\Post::where('title', null)->first();
-        // $this->assertDatabaseMissing($readPost);
+        $readPost = \App\Post::where('title', null )->first();
+        $this->assertNull($readPost);
         $readPost = \App\Post::where('text', 'bbb')->first();
         $this->assertNotNull($readPost);
+        $readPost = \App\Post::where('text', null)->first();
+        $this->assertNull($readPost);
         $readPost = \App\Post::where('user_id', '1')->first();
+        $this->assertNotNull($readPost);
+        $readPost = \App\Post::where('user_id', null)->first();
+        $this->assertNull($readPost);
+        $readPost = \App\Post::where('image_path', 'https://res.cloudinary.com/dueb2i48f/image/upload/c_fit,h_200,w_200/zsxizljjti5fxlfyklim.png')->first();
+        $this->assertNotNull($readPost);
+        $readPost = \App\Post::where('image_path', null )->first();
         $this->assertNotNull($readPost);
 
      }
